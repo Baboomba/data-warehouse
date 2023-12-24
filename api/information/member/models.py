e7d79d8c2690fed8d44a0ced1bac7ffe62804e5d
An Sung Eun <bach0918@outlook.kr>
2023-12-24 20:03:59 +0900

    altered member model

--- api/information/member/models.py
+++ api/information/member/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Info(models.Model):
-    product_info = models.CharField(max_length=20)
+    product_id = models.CharField(max_length=20)
     product_name = models.CharField(max_length=150)
     monthly_premium = models.IntegerField()
     policy_id = models.CharField(unique=True, max_length=20)
@@ -9,15 +9,15 @@ class Info(models.Model):
     user_name = models.CharField(max_length=150)
     user_contact = models.CharField(max_length=30)
     user_birth = models.CharField(max_length=30)
-    imei = models.IntegerField()
-    serial_number = models.IntegerField()
-    initial_call = models.DateField()
+    imei = models.BigIntegerField(null=True)
+    serial_number = models.CharField(max_length=20, null=True)
+    initial_call = models.DateField(null=True)
     date_joined = models.DateField()
-    date_closed = models.DateField()
-    insurance_company = models.CharField(max_length=5)
-    insurance_status = models.CharField(max_length=10)
-    untact_solution = models.IntegerField()
-    is_promoted = models.CharField(max_length=5)
+    date_closed = models.DateField(null=True)
+    insurance_company = models.CharField(max_length=5, null=True)
+    insurance_status = models.CharField(max_length=10, null=True)
+    untact_solution = models.BigIntegerField(null=True)
+    is_promoted = models.CharField(max_length=5, null=True)
     
     def __str__(self):
         return self.policy_id
